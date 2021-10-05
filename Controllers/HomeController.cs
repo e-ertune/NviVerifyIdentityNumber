cb4d77b054f23714c9e41dd4672e8813542f6dc9
e-ertune <emin.ertune@hotmail.com>
2021-10-05 09:26:15 +0300

    Initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using NviVerifyIdentityNumber.Models;
+using System;
+using System.Diagnostics;
+using System.Globalization;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace NviVerifyIdentityNumber.Controllers
+{
+    public class HomeController : Controller
+    {
+        public HomeController()
+        {
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(User user)
+        {
+            if (await Verify(user))
+            {
+                TempData["verifyResult"] = "Doğrulama başarılı.";
+            }
+            else
+            {
+                TempData["verifyResult"] = "Doğrulama başarısız.";
+            }
+            return RedirectToAction("Index", "Home");
+        }
+
+        public async Task<bool> Verify(User user)
+        {
+            HttpClient client = new HttpClient();
+            String str1 = @"
+            <soap12:Envelope xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance' xmlns:xsd='http://www.w3.org/2001/XMLSchema' xmlns:soap12='http://www.w3.org/2003/05/soap-envelope'>
+                <soap12:Body>
+                    <TCKimlikNoDogrula xmlns='http://tckimlik.nvi.gov.tr/WS'>
+                        <TCKimlikNo>" + user.IdentityNumber + "</TCKimlikNo>" +
+                        "<Ad>" + user.FirstName + "</Ad>" +
+                        "<Soyad>" + user.LastName + "</Soyad>" +
+                        "<DogumYili>" + user.BirthYear + "</DogumYili>" +
+                    "</TCKimlikNoDogrula>" +
+                "</soap12:Body>" +
+            "</soap12:Envelope>";
+
+            HttpContent content = new StringContent(str1, Encoding.UTF8, "application/soap+xml");
+            client.DefaultRequestHeaders.Add("SOAPAction", "http://tckimlik.nvi.gov.tr/WS/TCKimlikNoDogrula");
+            var response = await client.PostAsync("https://tckimlik.nvi.gov.tr/Service/KPSPublic.asmx", content);
+            var result = response.Content.ReadAsStringAsync().Result;
+
+            XmlDocument xml = new XmlDocument();
+            xml.LoadXml(result);
+            return Convert.ToBoolean(xml.ChildNodes[1].FirstChild.FirstChild.FirstChild.FirstChild.InnerText);
+        }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,10 @@
+﻿namespace NviVerifyIdentityNumber.Models
+{
+    public class User
+    {
+        public string IdentityNumber { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int BirthYear { get; set; }
+    }
+}
